5be32e186d3a0b9cbf700ee66e66be33d97c2a03
Nabila Khairunnisa <107665829+nabilaakhairunnisa@users.noreply.github.com>
2023-08-17 06:27:41 +0700

    Create 5-while.kt

---
+++ Dicoding/5 Control Flow/5-while.kt
@@ -0,0 +1,13 @@
+//Contoh While
+fun main() {
+    var counter = 1
+    while (counter <= 7){
+        println("Hello, World!")
+        counter++
+    }
+}
+
+/*
+counter awalnya bernilai 1, kemudian pada perulangan while, nilai counter terus bertambah 1 
+sehingga mencetak "Hello, World!" sebanyak kurang dari sama dengan 7
+*/
