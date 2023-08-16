c94b9addeb5dc4666469048bb8f2a44d59b9545e
Nabila Khairunnisa <107665829+nabilaakhairunnisa@users.noreply.github.com>
2023-08-17 06:29:52 +0700

    Create 5-do-while.kt

---
+++ Dicoding/5 Control Flow/5-do-while.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var counter = 1
+    do {
+        println("Hello, World!")
+        counter++
+    } while (counter <= 7)
+}
+
+/*
+Perintah akan dijalankan terlebih dahulu, baru kemudian mengecek kondisi
+*/
