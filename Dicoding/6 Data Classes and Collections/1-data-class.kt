9f53d5c807b2e4f86a0e074e7c8e619d8069d460
Nabila Khairunnisa <107665829+nabilaakhairunnisa@users.noreply.github.com>
2023-08-17 08:29:13 +0700

    Create 1-data-class.kt

---
+++ Dicoding/6 Data Classes and Collections/1-data-class.kt
@@ -0,0 +1,9 @@
+/*
+Data class tidak memiliki logika di dalamnya
+Digunakan untuk mengelola sebuah data
+Otomatis menghasilkan constructor, dan fungsi toString(), equals(), hashCode(), copy(), componentN().
+Harus memiliki setidaknya 1 parameter
+*/
+
+//Contoh Syntax
+data class User(val name : String, val age : Int)
