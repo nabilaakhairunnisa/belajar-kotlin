31d84ec11666bed502b08f1fac7e0e6527e2e856
Nabila Khairunnisa <107665829+nabilaakhairunnisa@users.noreply.github.com>
2023-08-17 07:37:43 +0700

    Create 8a-continue.kt

---
+++ Dicoding/5 Control Flow/8a-continue.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val listOfInt = listOf(1, 2, 3, null, 5, null, 7)
+ 
+    for (i in listOfInt) {
+        if (i == null) continue
+        print(i)
+    }
+}
+
+/*
+Output: 12357
+
+Keyword continue melewatkan iterasi jika kondisi terpenuhi
+*/
