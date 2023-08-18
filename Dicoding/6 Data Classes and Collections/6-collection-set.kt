e8e1e76a5aad812cd2ba3d33ae28800644ea9b8d
Nabila Khairunnisa <107665829+nabilaakhairunnisa@users.noreply.github.com>
2023-08-18 13:43:07 +0700

    Update 6-collection-set.kt
    hapus spasi

--- Dicoding/6 Data Classes and Collections/6-collection-set.kt
+++ Dicoding/6 Data Classes and Collections/6-collection-set.kt
@@ -1,27 +1,27 @@
 fun main(){
-	  val setA = setOf(1, 2, 4, 2, 1, 5)
+    val setA = setOf(1, 2, 4, 2, 1, 5)
     val setB = setOf(1, 2, 4, 5)
     val setC = setOf(1, 5, 7)
 
-    // Otomatis membuang angka yang duplikat
+    //Otomatis membuang angka yang duplikat
     println(setA)
-    // Output: [1, 2, 4, 5]
+    //Output: [1, 2, 4, 5]
 
-    // Mengecek apakah isi dari setA sama dengan setB
+    //Mengecek apakah isi dari setA sama dengan setB
     println(setA == setB)
-    // Output: true
+    //Output: true
 
-    // Mengecek apakah angka 5 termasuk dalam setA
+    //Mengecek apakah angka 5 termasuk dalam setA
     println(5 in setA)
-	  // Output: true
+    //Output: true
 
-    // Gabungan setA dan setC
+    //Gabungan setA dan setC
     val union = setA.union(setC)
     println(union)
-    // Output: [1, 2, 4, 5, 7]
+    //Output: [1, 2, 4, 5, 7]
     
-    // Irisan setA dengan setC
+    //Irisan setA dengan setC
     val intersect = setA.intersect(setC)
     println(intersect)
-    // Output: [1, 5]
+    //Output: [1, 5]
 }
