6691ed12708762b3d86bf946ac87e76822bb574d
Nabila Khairunnisa <107665829+nabilaakhairunnisa@users.noreply.github.com>
2023-08-22 14:45:02 +0700

    Update and rename 11-cara-akses-konteks.kt to 11-kotlin-standard-library.kt

---
+++ Dicoding/7 Kotlin Functional Programming/11-kotlin-standard-library.kt
@@ -0,0 +1,38 @@
+/*Kotlin Standard Library: memiliki beberapa function (scope function) untuk bagaimana menuliskan logika kode dalam objek 
+​
+Scope Fungction: Untuk mengeksekusi blok kode dalam sebuah objek.
+​
+Cara akses konteks dari sebuah objek:
+1. this (lambda receiver)
+   - run
+   - with
+   - apply
+  
+2. it (lambda argumen)
+   - let 
+   - also */
+​
+fun main() {
+   //Contoh Akses Konteks this (lambda receiver)
+   //StringBuilder().apply adalah cara akses konteks itu sendiri
+   //StringBuilder() adalah sebuah class
+   //apply{} merupakan lambda function
+   val buildString = StringBuilder().apply { 
+     //append adalah sebuah method dari class StringBuilder()
+     append("Hello ")
+     append("Kotlin")
+   }
+   println (buildString)
+   //Output: Hello Kotlin
+   
+   //Contoh Akses Konteks it (lambda argumen)
+   //Deklarasi variabel text yang berisi String Hello
+   val text = "Hello"
+   //let {} merupakan lambda function yang memiliki parameter bernama value 
+   //Mengakses dan mengambil nilai dari variabel text
+   text.let { value ->
+     val message = "$value Kotlin"
+     println(message)
+     //Output: Hello Kotlin
+   }
+}
