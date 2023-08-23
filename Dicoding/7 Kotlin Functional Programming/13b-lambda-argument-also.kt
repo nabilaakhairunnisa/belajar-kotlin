90f1ea6102c9004bd3003f232044cbfcd01125d3
Nabila Khairunnisa <107665829+nabilaakhairunnisa@users.noreply.github.com>
2023-08-23 23:09:33 +0700

    Update 13b-lambda-argument-also.kt

--- Dicoding/7 Kotlin Functional Programming/13b-lambda-argument-also.kt
+++ Dicoding/7 Kotlin Functional Programming/13b-lambda-argument-also.kt
@@ -3,8 +3,10 @@
 //menggunakan konten objek sebagai argumen di dalam blok lambda
 
 fun main() {
-    val text - "Hello Kotlin" //declare a variable text 
-    val result = text.also { //use also extension function on the text 
+    val text - "Hello Kotlin" //declare a variable text. create an instance of the 'string' class
+    val result = text.also { //use also extension function on the text. also takes a lambda as an argument 
+        //it provides the object text as the receiver for that lambda
+        //menyediakan objek (text) sebagai receiver untuk lambda
         println ("value length: ${it.length}")
     }
     println("text: $result")
