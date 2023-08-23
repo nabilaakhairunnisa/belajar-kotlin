f0637b9b02318c534b980f65482b87cc16cac647
Nabila Khairunnisa <107665829+nabilaakhairunnisa@users.noreply.github.com>
2023-08-23 23:11:51 +0700

    Update 13b-lambda-argument-also.kt

--- Dicoding/7 Kotlin Functional Programming/13b-lambda-argument-also.kt
+++ Dicoding/7 Kotlin Functional Programming/13b-lambda-argument-also.kt
@@ -1,6 +1,6 @@
 //also: similliar with apply, but doesn't modify the object.package
-//where you want to use th object's context as an argument within the lambda block
-//menggunakan konten objek sebagai argumen di dalam blok lambda
+//where you want to use th object's context as an argument within the lambda block without configure or modify the object itself
+//menggunakan konten objek sebagai argumen di dalam blok lambda tanpa mengkonfigurasi atau mengubah objek tersebut (nilainya)
 
 fun main() {
     val text - "Hello Kotlin" //declare a variable text. create an instance of the 'string' class
