af4acc05b37d668ce60753361f0a6cff5a9ad21e
Nabila Khairunnisa <107665829+nabilaakhairunnisa@users.noreply.github.com>
2023-08-25 07:44:32 +0700

    Update 18-recrusion.kt

--- Dicoding/7 Kotlin Functional Programming/18-recrusion.kt
+++ Dicoding/7 Kotlin Functional Programming/18-recrusion.kt
@@ -1,3 +1,5 @@
+//Recrusion Call: memanggil fungsi di dalam fungsi itu sendiri
+
 fun main() {
   println("Faktorial dari 5 adalah ${Factorial(5)}")
 }
@@ -6,20 +8,7 @@ fun Factorial(n: Int): Int {
   return if (n == 1) {
     n
   } else {
-    n * Factorial(n - 1) //anjay bisa manggil fungsi di dalam fungsi itu sendiri
-  }
-}
-
-//Tail Call Recursion: 
-  //pemanggilan rekrusif merupakan pernyataan terakhir dalam fungsi
-  //hasil dari pemanggilan rekrusif langsung dikembalikan tanpa melakukan operasi tambahan
-
-fun FactorialTailCall(n: Int, result: Int = 1): Int {
-  val newResult = n * result //mengalikan kedua parameter fungsi 
-  return if (n == 1) { //Jika n sama dengan 1, maka 
-    newResult
-  } else {
-    Factorial(n - 1, newResult) 
+    n * Factorial(n - 1) 
   }
 }
 
