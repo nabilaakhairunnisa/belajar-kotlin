007ff8ba58f420bc1769370934e98e0f0daf5913
Nabila Khairunnisa <107665829+nabilaakhairunnisa@users.noreply.github.com>
2023-08-26 00:32:29 +0700

    Create 14-interfaces.kt

---
+++ Dicoding/8 Kotlin Object-Oriented Programming/14-interfaces.kt
@@ -0,0 +1,2 @@
+//Merupakan konsep sifat umum yang digunakan oleh kelas agar dapat memiliki sifat tersebut
+//Tujuan: agar kelas yang mengimplementasikannya harus melakukan override seluruh properti dan fungsi
