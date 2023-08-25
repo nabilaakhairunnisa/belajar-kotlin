f894cd44a3bbda1c706e2c5d1a5df05c031eca8f
Nabila Khairunnisa <107665829+nabilaakhairunnisa@users.noreply.github.com>
2023-08-25 23:39:41 +0700

    Create 10-visibility-modifiers.kt

---
+++ Dicoding/8 Kotlin Object-Oriented Programming/10-visibility-modifiers.kt
@@ -0,0 +1,14 @@
+//Merupakan hak akses suatu kelas, objek, dsb
+
+  //Public: dapat diakses oleh semua anggota
+
+  //Private: tidak dapat diakses di luar scopenya. 
+    //Jika ingin mengaksesnya, tambahkan fungsi get set pada kelas tersebut
+    //Tidak bisa mengakses even kelasnya
+
+  //Protected: seperti private namun lebih luas.
+    //Hanya dapat diakses oleh kelas yang merupakan turunannya.
+    //Kelasnya tetap bisa diakses, namun propertinya tidak.
+
+  //Internal: hanya bisa diakses dari modul yang sama
+    
