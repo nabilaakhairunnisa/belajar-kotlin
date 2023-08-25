dfb9267946c17cd7f5a8b83efbc0a960c4672337
Nabila Khairunnisa <107665829+nabilaakhairunnisa@users.noreply.github.com>
2023-08-25 07:44:59 +0700

    Update 18-tail-call-recrusion.kt

--- Dicoding/7 Kotlin Functional Programming/18-tail-call-recrusion.kt
+++ Dicoding/7 Kotlin Functional Programming/18-tail-call-recrusion.kt
@@ -1,9 +1,17 @@
-//keyword trailrec: pemanggilan rekrisif adalah pernyataan terakhir dalam fungsi, dan hasil pemanggilan tersebut dikembalikan tanpa operasi tambahan
-tailrec fun factorial(n: Int, result: Int = 1) {
-  val newResult = n * result
-  result if (n == 1) {
+fun main() {
+  println("Faktorial dari 5 adalah ${FactorialTailCall(5)}")
+}
+
+//Tail Call Recursion: 
+  //pemanggilan rekrusif merupakan pernyataan terakhir dalam fungsi
+  //hasil dari pemanggilan rekrusif langsung dikembalikan tanpa melakukan operasi tambahan
+
+//kita punya parameter n dan result bertipe Int
+tailrec fun FactorialTailCall(n: Int, result: Int = 1): Int {
+  val newResult = n * result //n dikali 1
+  return if (n == 1) { //Jika n sama dengan 1, maka panggil variabel newResult
     newResult
   } else {
-    factorial(n - 1, newResult)
+    FactorialTailCall(n - 1, newResult) //Jika n selain 1 maka panggil fungsi FactorialTailCall    
   }
 }
