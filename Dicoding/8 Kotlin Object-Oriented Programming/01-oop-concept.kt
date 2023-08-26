a6fc29a57f2fe4baae8aafa973f3d1b911f64074
Nabila Khairunnisa <107665829+nabilaakhairunnisa@users.noreply.github.com>
2023-08-26 09:59:23 +0700

    Update 01-oop-concept.kt

--- Dicoding/8 Kotlin Object-Oriented Programming/01-oop-concept.kt
+++ Dicoding/8 Kotlin Object-Oriented Programming/01-oop-concept.kt
@@ -1,4 +1,11 @@
 //Class: Blueprint
+
 //Atribut/properties: Komponen yang dimiliki mobil (misalnya)
+
+//Instance: Misal suatu kelas digunakan sebagai pembuatan suatu objek
+  //Maka objek objek tersebutlah yang merupakan instance dari kelas tersebut
+
+//Function/Method
+
 //Object: Kemampuan yang dimiliki mobil. 
   //berfungsi untuk mengakses berbagai properti dan fungsi pada kelas
